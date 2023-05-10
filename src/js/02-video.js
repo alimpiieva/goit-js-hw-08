c2d827fc713619584e1f01a9cb6f896aac9492c6
eXis13 <127613464+eXis13@users.noreply.github.com>
2023-05-10 22:25:14 +0300

    hw#8 task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+const saveCurrentTime = () => {
+  player.getCurrentTime().then((time) => {
+    localStorage.setItem('videoplayer-current-time', time);
+  }).catch((error) => {
+    console.error('Error getting current time:', error);
+  });
+};
+
+const setCurrentTimeFromStorage = () => {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(parseFloat(currentTime)).catch((error) => {
+      console.error('Error setting current time:', error);
+    });
+  }
+};
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+setCurrentTimeFromStorage();
